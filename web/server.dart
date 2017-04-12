2c92d1a84cd84ac4ad34355ffbd3ffba08604233
Rechunk <gabri.marc@gmx.net>
2017-04-12 11:44:24 +0200

    Basic database interactions

--- web/server.dart
+++ web/server.dart
@@ -15,10 +15,35 @@ List<Discussion> allDiscussions = [
   new Discussion("Tom Fedel",new DateTime.now(), "Tests", "t vero eos et accusam?", comments)
 ];
 
+var database = null;
+
+void connectToDatabase(){
+  database = new ConnectionPool(
+    host: 'localhost', port: 3306,
+    user: 'root', password: 'password',
+    db: 'angularsite');
+}
+
 main() async {
+  connectToDatabase();
+
   _apiServer.addApi(new DiscussionAPI());
   HttpServer server = await HttpServer.bind("127.0.0.1", 8082);
   server.listen(_apiServer.httpRequestHandler);
+}
+
+getAllDiscussionsFromDb() async {
+
+  List<Discussion> discussions = [];
+
+  List<Discussion> returnDiscussions(List<Discussion> disc){
+    return discussions;
+  }
+
+  var results = await database.query("select * from discussions");
+  return results.forEach((disc){
+    discussions.add(new Discussion(disc[0], disc[1], disc[2], disc[3], comments));
+  }).then(returnDiscussions);
 }
 
 @ApiClass(
@@ -28,7 +53,7 @@ main() async {
 class DiscussionAPI {
   @ApiMethod(method: "GET", path: "getAllDiscussions")
   List<Discussion> getAllDiscussions() {
-      return allDiscussions;
+    return getAllDiscussionsFromDb();
   }
 
   @ApiMethod(method: "GET", path: "getMatchingDiscussions/{searchTerm}")
